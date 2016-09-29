aeec8d5c917472250bb44eddc6f70a08a9f6961c
Martijn Dekker <martijn@inlv.org>
2016-09-29 14:35:10 +0100

    mktemp -C: using 'command -p rm' for trap instead of just 'rm' is safer

--- libexec/modernish/sys/base/mktemp.mm
+++ libexec/modernish/sys/base/mktemp.mm
@@ -151,7 +151,7 @@ mktemp() {
 		fi
 		# On shells other than bash, ksh93 and mksh, EXIT traps are not executed on
 		# receiving a signal, so we have to trap the appropriate signals explicitly.
-		pushtrap "rm -rf ${_Msh_mTo_C}" INT PIPE TERM EXIT
+		pushtrap "command -p rm -rf ${_Msh_mTo_C}" INT PIPE TERM EXIT
 	fi
 	unset -v _Msh_mTo_d _Msh_mTo_Q _Msh_mTo_F _Msh_mTo_C || :	# BUG_UNSETFAIL compat
 }
