modernish/modernish

commit 0fde9cd76a5451197ac174eae19e91e6d42b0596

{,un}install.sh: simplify: don't modify PATH, just 'cd'

install.sh
@@ -31,16 +31,11 @@ case $0 in
 ( * )	srcdir=. ;;
 esac
 srcdir=$(cd "$srcdir" && pwd -P) || exit
-
-# make bin/modernish findable in $PATH
-case $PATH in
-( "$srcdir"/bin:* ) ;;
-( * ) PATH=$srcdir/bin:$PATH ;;
-esac
+cd "$srcdir" || exit
 
 # commands for test-initialising modernish
 # test thisshellhas(): a POSIX reserved word, POSIX special builtin, and POSIX regular builtin
-test_modernish='. modernish || exit
+test_modernish='. bin/modernish || exit
 thisshellhas --rw=if --bi=set --bi=wait || exit 1 "Failed to determine a working thisshellhas() function."'
 
 # try to test-initialize modernish in a subshell to see if we can run it
@@ -64,7 +59,7 @@ if ( eval '[[ -n ${.sh.version} && -n ${MSH_VERSION+s} ]]' ) 2>/dev/null; then
 fi
 
 # load modernish and some modules
-. modernish
+. bin/modernish
 use safe -w BUG_APPENDC -w BUG_UPP	# IFS=''; set -f -u -C (declaring compat with bugs)
 use var/setlocal -w BUG_FNSUBSH		# setlocal is like zsh anonymous functions
 use var/arith/cmp			# arithmetic comparison shortcuts: eq, gt, etc.
@@ -319,20 +314,20 @@ fi
 # Parameter: $1 = full source path for a file or directory.
 # TODO: handle symlinks (if/when needed)
 install_handler() {
-	case ${1#"$srcdir"} in
+	case ${1#.} in
 	( */.* | */_* | */Makefile | *~ | *.bak )
 		# ignore these (if directory, prune)
 		return 1 ;;
 	esac
 	if is dir $1; then
-		absdir=${1#"$srcdir"}
+		absdir=${1#.}
 		destdir=$installroot$absdir
 		if not is present $destdir; then
 			echo "- Creating directory: $destdir"
 			mkdir -p $destdir
 		fi
 	elif is reg $1; then
-		relfilepath=${1#"$srcdir"/}
+		relfilepath=${1#./}
 		if not contains $relfilepath /; then
 			# ignore files at top level
 			return 1
@@ -371,7 +366,7 @@ install_handler() {
 }
 
 # Traverse through the source directory, installing files as we go.
-traverse $srcdir install_handler
+traverse . install_handler
 
 # If we're on zsh, install compatibility symlink.
 if isset ZSH_VERSION && isset my_zsh && isset zsh_compatdir; then

uninstall.sh
@@ -31,12 +31,10 @@ case $0 in
 ( * )	srcdir=. ;;
 esac
 srcdir=$(cd "$srcdir" && pwd -P) || exit
-
-# make bin/modernish findable in $PATH
-PATH=$srcdir/bin:$PATH
+cd "$srcdir" || exit
 
 # try to test-initialize modernish in a subshell to see if we can run it
-if ! ( . modernish ); then
+if ! ( . bin/modernish ); then
 	echo
 	echo "The shell executing this script can't run modernish. Try running uninstall.sh"
 	echo "with a more fully POSIX-compliant shell, for instance: dash uninstall.sh"
@@ -49,7 +47,7 @@ fi 1>&2
 alias BUG_ALSUBSH >/dev/null 2>&1 && unalias -a
 
 # load modernish and some modules
-. modernish
+. bin/modernish
 use safe -w BUG_APPENDC -w BUG_UPP	# IFS=''; set -f -u -C (declaring compat with bugs)
 use var/arith/cmp			# arithmetic comparison shortcuts: eq, gt, etc.
 use loop/select -w BUG_SELECTEOF	# ksh/zsh/bash 'select' now on all POSIX shells
@@ -64,9 +62,8 @@ harden rmdir
 
 # detect existing modernish installations from $PATH, storing their install
 # prefixes in the positional parameters (strip 2 path elements: /bin/modernish)
-which -aQsP2 modernish || exit 1 "Internal error: Cannot find modernish!"
+which -aQsP2 modernish
 eval "set -- $REPLY"	# which -Q gives shellquoted output for safe 'eval'
-shift			# skip the first one: it's our source directory
 
 unset -v installroot
 while not isset installroot; do
@@ -117,14 +114,14 @@ is dir $zcsd && not is nonempty $zcsd && rmdir $zcsd
 # Handler function for 'traverse': uninstall one file, remembering directories.
 # Parameter: $1 = full source path for a file or directory.
 uninstall_handler() {
-	case ${1#"$srcdir"} in
+	case ${1#.} in
 	( */.* | */_* | */Makefile | *~ | *.bak )
 		# ignore these
 		return 1 ;;
 	esac
 
 	if is reg $1; then
-		relfilepath=${1#"$srcdir"/}
+		relfilepath=${1#./}
 		if not contains $relfilepath /; then
 			# ignore files at top level
 			return 1
@@ -134,8 +131,8 @@ uninstall_handler() {
 			echo "- Removing: $destfile "
 			rm -f $destfile
 		fi
-	elif is dir $1 && not identic $1 $srcdir; then
-		absdir=${1#"$srcdir"}
+	elif is dir $1 && not identic $1 .; then
+		absdir=${1#.}
 		destdir=$installroot$absdir
 		if is nonempty $destdir; then
 			echo "- Leaving non-empty directory $destdir"
@@ -148,6 +145,6 @@ uninstall_handler() {
 
 # Traverse depth-first through the source directory, uninstalling
 # corresponding destination files and directories as we go.
-traverse -d $srcdir uninstall_handler
+traverse -d . uninstall_handler
 
 print '' "Modernish $MSH_VERSION was uninstalled successfully from $installroot."

commit 3b3dbd6a96aa84eb60a1deadd61aac6321b89b87

install.sh: fix relaunching when install.sh not in $PWD
Commit 0fde9cd introduced a bug that caused relaunching the installer to
fail if was originally launched from a present working directory other than
the one install.sh resides in. This commit fixes that bug.

install.sh
@@ -174,7 +174,7 @@ pick_shell_and_relaunch() {
 	endlocal
 
 	print "* Relaunching installer with $msh_shell" ''
-	exec env MSH_SHELL=$msh_shell $msh_shell $0 --relaunch
+	exec env MSH_SHELL=$msh_shell $msh_shell $srcdir/${0##*/} --relaunch
 }
 
 # Simple function to ask a question of a user.