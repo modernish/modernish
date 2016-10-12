2764c974922640c00a5edae173c9b16f33b485da
Martijn Dekker <martijn@inlv.org>
2016-10-12 06:45:27 +0200

    loop/with: optimisation: use ARITHCMD if possible

--- libexec/modernish/loop/with.mm
+++ libexec/modernish/loop/with.mm
@@ -42,6 +42,65 @@
 
 alias with='_Msh_with_init=y && while _Msh_doWith'
 
+if thisshellhas ARITHCMD; then
+
+# Using ksh/zsh/bash arithmetic commands, if available, is the fastest and
+# most straightforward. Must 'eval' it to avoid a syntax error on shells without ARITHCMD.
+
+eval '_Msh_doWith() {
+	case ${_Msh_with_init-} in
+	( "${1-},${3-},${5-}" )
+		(((${_Msh_with_var}+=_Msh_with_inc)${_Msh_with_cmp}${3}))
+		return
+	esac
+
+	case ${_Msh_with_init+s},${#},${2-},${4-},${1-} in
+	( s,[35],to,*,=* | s,[35],to,*,[0123456789]*=* | s,[35],to,*,*[!${ASCIIALNUM}_]*=* )
+		die "with: invalid variable name: '\''${1%=*}'\''" || return ;;
+	( s,3,to,,*=* )
+		_Msh_with_var=${1%=*}
+		isint "${1##*=}" || die "with: assignment: integer value expected, got '\''${1##*=}'\''" || return
+		isint "$3" || die "with: to: integer value expected, got '\''$3'\''" || return
+		((_Msh_with_inc = ${1##*=} > $3 ? -1 : 1)) ;;
+	( s,5,to,step,*=* )
+		_Msh_with_var=${1%=*}
+		isint "${1##*=}" || die "with: assignment: integer value expected, got '\''${1##*=}'\''" || return
+		isint "$3" || die "with: to: integer value expected, got '\''$3'\''" || return
+		isint "$5" || die "with: step: integer value expected, got '\''$5'\''" || return
+		((_Msh_with_inc = $5)) ;;
+	( s,[35],to,* )
+		die "with: syntax error: assignment expected" || return ;;
+	( s,2,to,,* )
+		die "with: to: integer value expected" || return ;;
+	( s,4,to,step,* )
+		die "with: step: integer value expected" || return ;;
+	( s,[45],to,* )
+		die "with: syntax error: '\''step'\'' expected, got '\''$4'\''" || return ;;
+	( s,?[!,]* | s,[6789],* )
+		die "with: syntax error: excess arguments" || return ;;
+	( s,?,* )
+		die "with: syntax error: '\''to'\'' expected${2+, got '\''$2'\''}" || return ;;
+	( * )
+		die "with: init: internal error" || return ;;
+	esac
+
+	case ${_Msh_with_inc} in
+	( -* )	_Msh_with_cmp=">=" ;;
+	( * )	_Msh_with_cmp="<=" ;;
+	esac
+
+	case ${_Msh_with_init} in
+	( y )	(($1)) ;;				# loop init
+	( * )	((${_Msh_with_var}+=_Msh_with_inc)) ;;	# loop re-entry
+	esac
+
+	_Msh_with_init=$1,$3,${5-}
+
+	((${_Msh_with_var}${_Msh_with_cmp}${3}))
+}'
+
+else
+
 # Since we have full POSIX arithmetics with assignment and comparison, we
 # don't need "eval" at all. Avoiding repeated shell grammar parsing while
 # using arith to combine the assignment and the comparison is much faster.
@@ -98,3 +157,5 @@ _Msh_doWith() {
 
 	return "$((${_Msh_with_var}${_Msh_with_cmp}${3}))"
 }
+
+fi
