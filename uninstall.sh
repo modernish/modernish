aa22801db94409ef2abc70b3b2bd19f85f18ba45
Martijn Dekker <martijn@inlv.org>
2016-10-20 06:50:39 +0200

    uninstall.sh: support non-interactive operation
    Command line options for uninstall.sh:
    
    usage: ./uninstall.sh [ -n ] [ -f ] [ -d INSTALLROOT ]
    	-n: non-interactive operation
    	-f: delete */modernish directories even if files left
    	-d: specify root directory of modernish installation to uninstall

--- uninstall.sh
+++ uninstall.sh
@@ -33,6 +33,29 @@ esac
 srcdir=$(cd "$srcdir" && pwd -P) || exit
 cd "$srcdir" || exit
 
+usage() {
+	echo "usage: $0 [ -n ] [ -f ] [ -d INSTALLROOT ]"
+	echo "	-n: non-interactive operation"
+	echo "	-f: delete */modernish directories even if files left"
+	echo "	-d: specify root directory of modernish installation to uninstall"
+	exit 1
+} 1>&2
+
+# parse options
+unset -v opt_n opt_f installroot
+while getopts 'nfd:' opt; do
+	case $opt in
+	( \? )	usage ;;
+	( n )	opt_n='' ;;		# non-interactive operation
+	( f )	opt_f='' ;;		# force: delete */modernish/* directories if content left
+	( d )	installroot=$OPTARG ;;	# directory: specify install root directory
+	esac
+done
+shift $(($OPTIND - 1))
+case $# in
+( [!0]* ) usage ;;
+esac
+
 # try to test-initialize modernish in a subshell to see if we can run it
 #
 # On ksh93, subshells are normally handled specially without forking. Depending
@@ -40,7 +63,7 @@ cd "$srcdir" || exit
 # subshell into the main shell (e.g. aliases, see BUG_ALSUBSH). This may
 # prevent the proper init of modernish later. To circumvent this problem, force
 # the forking of a real subshell by making it a background job.
-if ! { (eval "$test_modernish") & wait "$!"; }; then
+if ! { (eval ". modernish") & wait "$!"; }; then
 	echo
 	echo "The shell executing this script can't run modernish. Try running uninstall.sh"
 	echo "with a more fully POSIX-compliant shell, for instance: dash uninstall.sh"
@@ -51,39 +74,57 @@ fi 1>&2
 . bin/modernish
 use safe -w BUG_APPENDC -w BUG_UPP	# IFS=''; set -f -u -C (declaring compat with bugs)
 use var/arith/cmp			# arithmetic comparison shortcuts: eq, gt, etc.
-use loop/select -w BUG_SELECTEOF	# ksh/zsh/bash 'select' now on all POSIX shells
+use loop/select -w BUG_SELECTEOF \
+		-w BUG_SELECTRPL	# ksh/zsh/bash 'select' now on all POSIX shells
 use sys/base/which			# for modernish version of 'which'
-use sys/dir/traverse			# for 'traverse'
+use sys/dir				# for 'traverse' and 'countfiles'
 use var/string				# for 'replacein'
 use sys/user/id -f			# for $UID (and $USER)
 
-# abort program if any of these commands give an error
-harden rm
-harden rmdir
+# abort program if any of these commands give an error; trace 'rm' and 'rmdir'
+harden -t rm
+harden -t rmdir
+harden ls
+
+# validate options
+if isset installroot; then
+	is -L dir $installroot || exit 1 "not a directory: $installroot"
+fi
 
 # detect existing modernish installations from $PATH, storing their install
 # prefixes in the positional parameters (strip 2 path elements: /bin/modernish)
 which -aQsP2 modernish
 eval "set -- $REPLY"	# which -Q gives shellquoted output for safe 'eval'
 
-unset -v installroot
-while not isset installroot; do
+while not isset installroot || not is -L dir $installroot; do
 	if gt $# 0; then
-		# we detected existing installations: present a menu
-		print "* Choose the directory prefix from which to uninstall modernish,"
-		print "  or enter another prefix (starting with '/')."
-		REPLY=''  # BUG_SELECTEOF workaround
-		select installroot; do
-			if empty $installroot && startswith $REPLY /; then
-				installroot=$REPLY
+		if isset opt_n; then
+			# non-interactive mode: uninstall the first one found
+			installroot=$1
+		else
+			# we detected existing installations: present a menu
+			printf '* Choose the directory prefix from which to uninstall modernish'
+			if thisshellhas BUG_SELECTRPL; then
+				printf '.\n'
+			else
+				printf ',\n  or enter another prefix (starting with '/').\n'
 			fi
-			if not empty $installroot; then
-				break
-			fi
-		done
-		empty $REPLY && exit 2 Aborting.	# user pressed ^D
+			REPLY=''  # BUG_SELECTEOF workaround
+			select installroot; do
+				if empty $installroot && startswith $REPLY /; then
+					installroot=$REPLY
+				fi
+				if not empty $installroot; then
+					break
+				fi
+			done
+			empty $REPLY && exit 2 Aborting.	# user pressed ^D
+		fi
 	else
 		# we did not detect existing installations
+		if isset opt_n; then
+			exit 1 "No existing modernish installation was found in your PATH."
+		fi
 		print "* No existing modernish installation was found in your PATH." \
 		      "  Enter the directory prefix from which to uninstall modernish."
 		if eq UID 0; then
@@ -100,52 +141,69 @@ while not isset installroot; do
 	fi
 	if not is present $installroot; then
 		echo "$installroot doesn't exist. Please try again."
-		unset -v installroot
 	elif not is -L dir $installroot; then
 		print "$installroot is not a directory. Please try again."
-		unset -v installroot
 	fi
 done
 
 # Remove zsh compatibility symlink, if present.
 zcsd=$installroot/libexec/modernish/zsh-compat
-is sym $zcsd/sh && rm -f $zcsd/sh
+is sym $zcsd/sh && rm $zcsd/sh
 is dir $zcsd && not is nonempty $zcsd && rmdir $zcsd
 
 # Handle README.md specially.
 if is reg $installroot/share/doc/modernish/README.md; then
-	echo "- Removing: $installroot/share/doc/modernish/README.md"
-	rm -f $installroot/share/doc/modernish/README.md
+	rm $installroot/share/doc/modernish/README.md
 fi
 
+# Flag to remember whether we've actually done anything. This is so we
+# don't give an "uninstalled successfully" message if nothing happened.
+unset -v flag
+
 # Handler function for 'traverse': uninstall one file or directory.
+# If option -f was given, remove */modernish/* and */modernish directories even if files are left.
 # Directories must be emptied first, so use depth-first traversal.
 # Parameter: $1 = full source path for a file or directory.
 uninstall_handler() {
-	case ${1#.} in
+	case ${1#"$srcdir"} in
 	( */.* | */_* | */Makefile | *~ | *.bak )
 		# ignore these
 		return 1 ;;
 	esac
 
 	if is reg $1; then
-		relfilepath=${1#./}
+		relfilepath=${1#"$srcdir"/}
 		if not contains $relfilepath /; then
 			# ignore files at top level
 			return 1
 		fi
 		destfile=$installroot/$relfilepath
 		if is reg $destfile; then
-			echo "- Removing: $destfile "
-			rm -f $destfile
+			flag=
+			rm $destfile	# no 'rm -f', please; we want to stop on error.
 		fi
-	elif is dir $1 && not identic $1 .; then
-		absdir=${1#.}
+	elif is dir $1 && not identic $1 $srcdir; then
+		absdir=${1#"$srcdir"}
 		destdir=$installroot$absdir
-		if is nonempty $destdir; then
-			echo "- Leaving non-empty directory $destdir"
+		if isset opt_f && is dir $destdir && { contains $destdir '/modernish/' || endswith $destdir '/modernish'; }
+		then	# option -f: delete directories ending with */modernish regardless of their contents
+			if is nonempty $destdir; then
+				countfiles -s $destdir
+				echo "- WARNING: $REPLY stray item(s) left in $destdir, '-f' given, deleting anyway:"
+				ls -lA $destdir
+			fi
+			flag=
+			rm -r $destdir	# no 'rm -rf', please; we want to stop on error.
+		elif is nonempty $destdir; then
+			countfiles -s $destdir
+			if contains $destdir '/modernish/' || endswith $destdir '/modernish'; then
+				echo "- Warning: keeping $REPLY stray item(s) in $destdir:"
+				ls -lA $destdir
+			else
+				echo "- Keeping non-empty directory $destdir ($REPLY item(s) left)"
+			fi
 		elif is dir $destdir; then
-			echo "- Removing empty directory $destdir"
+			flag=
 			rmdir $destdir
 		fi
 	fi
@@ -153,6 +211,11 @@ uninstall_handler() {
 
 # Traverse depth-first through the source directory, uninstalling
 # corresponding destination files and directories as we go.
-traverse -d . uninstall_handler
+# This strategy ensures we don't delete anything we shouldn't.
+traverse -d $srcdir uninstall_handler
 
-print '' "Modernish $MSH_VERSION was uninstalled successfully from $installroot."
+if isset flag; then
+	print '' "Modernish $MSH_VERSION was uninstalled successfully from $installroot."
+else
+	exit 1 "No modernish installation found at $installroot."
+fi
