8ae0fe4ce5013ac7f6f240935c7b98d10d116d5a
Martijn Dekker <martijn@inlv.org>
2017-06-30 02:31:44 +0200

    testshells.sh: keep config file with list of shells
    share/doc/modernish/examples/testshells.sh:
    - No longer automatically detect shells every time; instead
      do the auto-detection the first time, storing the result
      in an editable configuration file.

--- share/doc/modernish/examples/testshells.sh
+++ share/doc/modernish/examples/testshells.sh
@@ -1,9 +1,12 @@
 #! /usr/bin/env modernish
 #! use safe -w BUG_APPENDC
 #! use sys/base/which
+#! use sys/base/rev -w BUG_MULTIBYTE
+#! use var/setlocal
 harden -p -e '> 1' grep
 harden -p -e '== 2 || > 4' tput
 harden -p printf
+harden -p sort
 
 unexport POSIXLY_CORRECT
 
@@ -33,6 +36,18 @@ if isset opt_t && not thisshellhas time; then
 	# harden external 'time' command against command not executable (126) or not found (127)
 	harden -e '==126 || ==127' time
 fi
+
+# Simple function to ask a question of a user.
+yesexpr=$(PATH=$DEFPATH exec locale yesexpr 2>/dev/null) || yesexpr=^[yY].*
+match $yesexpr \"*\" && yesexpr=${yesexpr#\"} && yesexpr=${yesexpr%\"}	# one buggy old 'locale' command adds spurious quotes
+ask_q() {
+	REPLY=''
+	while empty $REPLY; do
+		put "$1 "
+		read -r REPLY || exit 2 Aborting.
+	done
+	ematch $REPLY $yesexpr
+}
 
 # determine terminal capabilities (none if stdout (FD 1) is not on a terminal)
 if is onterminal 1; then
@@ -73,36 +88,45 @@ fi #2>/dev/null	# redirecting stderr to /dev/null here prevents 'tput cols' abov
 export COLUMNS
 
 # find shells
-which -as sh ash bash dash yash zsh zsh5 ksh ksh93 pdksh mksh lksh oksh
-shells_to_test=$REPLY	# newline-separated list of shells to test
-# supplement 'which' results with any additional shells from /etc/shells
-if can read /etc/shells; then
-	shells_to_test=${shells_to_test}${CCn}$(grep -E '^/[a-z/][a-z0-9/]+/[a-z]*sh[0-9]*$' /etc/shells |
-		grep -vE '(csh|/esh|/psh|/posh|/fish|/r[a-z])')
+shellsfile=~/.config/modernish/testshellsrc
+if is -L reg $shellsfile; then
+	shells_to_test=$(grep -v '#' $shellsfile)
+else
+	harden -ptc mkdir -p -m700 ${shellsfile%/*}
+	put "First run. Gathering shells into $shellsfile... "
+	which -as sh ash bash dash yash zsh zsh5 ksh ksh93 pdksh mksh lksh oksh
+	shells_to_test=$REPLY	# newline-separated list of shells to test
+	# supplement 'which' results with any additional shells from /etc/shells
+	if can read /etc/shells; then
+		shells_to_test=${shells_to_test}${CCn}$(grep -E '^/[a-z/][a-z0-9/]+/[a-z]*sh[0-9]*$' /etc/shells |
+			grep -vE '(csh|/esh|/psh|/posh|/fish|/r[a-z])')
+	fi
+	putln "# List of shells for testshells.sh" >|$shellsfile
+	putln $shells_to_test | rev | sort -u | rev >>$shellsfile
+	putln "Done." "Edit that file to your liking, or delete it to search again."
+	if ask_q "Edit it now?"; then
+		{ setlocal --dosplit	# $VISUAL or $EDITOR may contain arguments; must split
+			${VISUAL:-${EDITOR:-vi}} "$shellsfile" && shells_to_test=$(grep -v '#' "$shellsfile")
+		endlocal } || exit 1 "Drat. Your editor failed."
+	fi
+	putln "Commencing regular operation."
 fi
 
-IFS=$CCn
-shells_found=''
-for shell in $shells_to_test; do
-	# eliminate duplicates (symlinks, hard links)
-	for alreadyfound in $shells_found; do
-		if is samefile $shell $alreadyfound; then
-			continue 2
+{ setlocal --split=$CCn
+	for shell in $shells_to_test; do
+		can exec $shell || continue
+
+		printf '%s%24s: %s' "$tBlue" $shell "$tReset"
+		if isset opt_t; then
+			time $shell $script "$@"
+		else
+			$shell $script "$@"
+		fi
+		e=$?
+		if let e==0; then
+			printf '%s%s[%3d]%s\n' "$tEOL" "$tGreen" $e "$tReset"
+		else
+			printf '%s%s[%3d]%s\n' "$tEOL" "$tRed" $e "$tReset"
 		fi
 	done
-	can exec $shell || continue
-	shells_found=$shells_found$shell$CCn
-
-	printf '%s%24s: %s' "$tBlue" $shell "$tReset"
-	if isset opt_t; then
-		time $shell $script "$@"
-	else
-		$shell $script "$@"
-	fi
-	e=$?
-	if let e==0; then
-		printf '%s%s[%3d]%s\n' "$tEOL" "$tGreen" $e "$tReset"
-	else
-		printf '%s%s[%3d]%s\n' "$tEOL" "$tRed" $e "$tReset"
-	fi
-done
+endlocal }
