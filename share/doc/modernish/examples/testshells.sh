14d40a288c90a1f2406afa107b17f2927ad307bc
Martijn Dekker <martijn@inlv.org>
2018-11-06 22:55:19 +0000

    testshells: add -P (run script in POSIX mode) option; some cleanup
    share/doc/modernish/examples/testshells.sh:
    - Add option -P that causes the test script (whether specified by
      file or as code using the -c option) to run in POSIX mode on each
      shell running the script, if the shell in question supports it.
    - Most of the modules loaded and commands hardened were needed only
      for initialisation. So separate off the first-run initialisation
      routine into a subshell that loads these in the subshell only.
      This improves startup performance after first-run initialisation.
      It also serves as an example to demonstrate how modules may be
      used locally to subshells (as long as they don't depend on fancy
      aliases, like var/setlocal does).

--- share/doc/modernish/examples/testshells.sh
+++ share/doc/modernish/examples/testshells.sh
@@ -1,14 +1,8 @@
 #! /usr/bin/env modernish
 #! use safe -w BUG_APPENDC
-#! use sys/base/which
-#! use sys/base/rev
-#! use sys/term/readkey
 #! use var/setlocal
-#! use var/string
-harden -p -e '> 1' grep
 harden -p -e '== 2 || > 4' tput
 harden -p printf
-harden -p sort
 
 unexport POSIXLY_CORRECT
 
@@ -17,17 +11,19 @@ unexport POSIXLY_CORRECT
 # parse options
 showusage() {
 	putln "\
-Usage:	${ME##*/} [ -t ] SCRIPTFILE [ ARGUMENT ... ]
-	${ME##*/} [ -t ] -c COMMANDS [ ME_NAME [ ARGUMENT ... ] ]
+Usage:	${ME##*/} [ -t ] [ -P ] SCRIPTFILE [ ARGUMENT ... ]
+	${ME##*/} [ -t ] [ -P ] -c COMMANDS [ ME_NAME [ ARGUMENT ... ] ]
 	-t: time script execution
-	-c: specify script commands directly"
+	-c: specify script commands directly
+	-P: run script in POSIX mode (where supported)"
 }
-unset -v opt_t opt_c
-while getopts ':tc' opt; do
+unset -v opt_t opt_c opt_P
+while getopts ':tcP' opt; do
 	case $opt in
 	( \? )	exit -u 1 "Invalid option: -$OPTARG" ;;
 	( t )	opt_t='' ;;
 	( c )	opt_c='' ;;
+	( P )	opt_P='' ;;
 	esac
 done
 shift $(($OPTIND - 1))
@@ -55,17 +51,6 @@ if isset opt_t && not thisshellhas time; then
 	# harden external 'time' command against command not executable (126) or not found (127)
 	harden -e '==126 || ==127' time
 fi
-
-# Simple function to ask a question of a user.
-yesexpr=$(PATH=$DEFPATH command locale yesexpr 2>/dev/null) && trim yesexpr \" || yesexpr=^[yY]
-noexpr=$(PATH=$DEFPATH command locale noexpr 2>/dev/null) && trim noexpr \" || noexpr=^[nN]
-ask_q() {
-	REPLY=''
-	put "$1 (y/n) "
-	readkey -E "($yesexpr|$noexpr)" REPLY || exit 2 Aborting.
-	putln $REPLY
-	ematch $REPLY $yesexpr
-}
 
 # determine terminal capabilities (none if stdout (FD 1) is not on a terminal)
 if is onterminal 1; then
@@ -97,23 +82,69 @@ fi
 # find shells
 shellsfile=$MSH_CONFIG/shellsrc
 if not is -L reg $shellsfile; then
-	harden -ptc mkdir -p -m700 $MSH_CONFIG
-	put "First run. Gathering shells into $shellsfile... "
-	putln "# List of shells for testshells.sh. Arguments and shell grammar are supported." >|$shellsfile
-	{
-		which -q -a sh ash bash dash yash zsh zsh5 ksh ksh93 pdksh mksh lksh oksh
-		# supplement 'which' results with any additional shells from /etc/shells
-		if can read /etc/shells; then
-			grep -E '/([bdy]?a|pdk|[mlo]?k|z)?sh[0-9._-]*$' /etc/shells
+	putln "shellsrc not found; initialising it. Welcome to testshells."
+	# run all this in a subshell using some extra modules & hardened commands we don't need elsewhere
+	(
+		use sys/base/which
+		use sys/base/rev
+		use sys/term/readkey
+		use var/string
+		harden -p -e '> 1' grep
+		harden -pt mkdir
+		harden -p LC_COLLATE=C sort
+
+		# Simple function to ask a question of a user.
+		yesexpr=$(PATH=$DEFPATH command locale yesexpr 2>/dev/null) && trim yesexpr \" || yesexpr=^[yY]
+		noexpr=$(PATH=$DEFPATH command locale noexpr 2>/dev/null) && trim noexpr \" || noexpr=^[nN]
+		ask_q() {
+			REPLY=''
+			put "$1 (y/n) "
+			readkey -E "($yesexpr|$noexpr)" REPLY || exit 2 Aborting.
+			putln $REPLY
+			ematch $REPLY $yesexpr
+		}
+
+		mkdir -p -m700 $MSH_CONFIG
+		put "Gathering shells into $shellsfile... "
+		putln "# List of shells for testshells.sh. Arguments and shell grammar are supported." >|$shellsfile
+		{
+			which -q -a sh ash bash dash yash zsh zsh5 ksh ksh93 pdksh mksh lksh oksh
+			# supplement 'which' results with any additional shells from /etc/shells
+			if can read /etc/shells; then
+				grep -E '/([bdy]?a|pdk|[mlo]?k|z)?sh[0-9._-]*$' /etc/shells
+			fi
+		} | rev | sort -u | rev >>$shellsfile
+		putln "Done." "Edit that file to your liking, or delete it to search again."
+		if ask_q "Edit it now?"; then
+			# $VISUAL or $EDITOR may contain arguments; must split
+			setlocal --split -- ${VISUAL:-${EDITOR:-vi}}; do
+				"$@" $shellsfile
+			endlocal || exit 1 "Drat. Your editor failed."
 		fi
-	} | rev | sort -u | rev >>$shellsfile
-	putln "Done." "Edit that file to your liking, or delete it to search again."
-	if ask_q "Edit it now?"; then
-		setlocal --split -- ${VISUAL:-${EDITOR:-vi}}; do	# $VISUAL or $EDITOR may contain arguments; must split
-			"$@" $shellsfile
-		endlocal || exit 1 "Drat. Your editor failed."
+		putln "Commencing regular operation."
+	) || exit
+fi
+
+# make script set POSIX option if requested
+if isset opt_P; then
+	posixcode='
+		export POSIXLY_CORRECT=y
+		case ${ZSH_VERSION+s} in
+		( s )	emulate -R sh; setopt POSIX_ARGZERO MULTIBYTE 2>/dev/null; PS4="+ " ;;
+		( * )	(set -o posix) 2>/dev/null && set -o posix ;;
+		esac
+	'
+	if isset opt_c; then
+		script=$posixcode$CCn$script
+	else
+		use sys/base/mktemp
+
+		mktemp -tsCC testshells_script  # note: auto-cleanup
+		{ putln	$posixcode
+		  harden -p -c cat $script
+		} > $REPLY || die "can't write to $REPLY"
+		script=$REPLY
 	fi
-	putln "Commencing regular operation."
 fi
 
 # parse shell grammar in $1, and check if the command is a shell
