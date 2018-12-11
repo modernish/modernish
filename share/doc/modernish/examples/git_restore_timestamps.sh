67fbe49bf36235d8e1ce6470ba055a15caf9643f
Martijn Dekker <martijn@inlv.org>
2018-12-11 00:19:48 +0000

    add new example program: git repo timestamp restorer
    share/doc/modernish/examples/git_restore_timestamps.sh:
    - Added. A handy program to set the timestamps of your Git repo
      working directory files to the time of the last commit
      referencing them, so that 'ls -lt' (sort by date) provides useful
      output, etc.
      (Not recommended if you have to build object code based on file
      tiemstamps, such as with 'make'. But that is not applicable to
      scripting projects such as modernish.)

---
+++ share/doc/modernish/examples/git_restore_timestamps.sh
@@ -0,0 +1,33 @@
+#! /usr/bin/env modernish
+#! use safe
+#! use var/loop
+harden git
+harden -e '>1' -f wd_is_clean git diff-index --quiet HEAD
+harden -pt touch
+
+# Git timestamp restorer. This sets the timestamps on working directory
+# files within a local Git repository to the date of the last commit in
+# which they were changed. If you first change to a subdirectory of the
+# repo, this will only restore the timestamps down from that directory.
+
+if not wd_is_clean; then
+	exit 1 'Working directory not clean. Commit or stash changes first.'
+fi
+
+LOOP for --split=$CCn repofile in $(git ls-tree -r -t --name-only HEAD)
+DO
+	if startswith $repofile '../'; then
+		# If we're in a subdirectory of the repo, skip parent dirs.
+		continue
+	elif not is present $repofile; then
+		die "File $repofile not in WD! (should never happen)"
+	fi
+
+	# Ask Git for latest commit's timestamp formatted for POSIX 'touch -t'.
+	timestamp=$(git log --format=%cd \
+		--date=format:%Y%m%d%H%M.%S \
+		-1 HEAD -- $repofile)
+
+	# The 'touch' command is traced due to 'harden -t' above.
+	touch -t $timestamp $repofile
+DONE
