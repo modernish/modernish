4d1dbacb95f92ca3927f727dcd1e2ab2dc565b12
Martijn Dekker <martijn@inlv.org>
2019-01-14 03:18:22 +0100

    good.sh: unbreak if MSH_FTL_DEBUG is set (re: 09d3d953)
    lib/_install/good.sh:
    - Commit 09d3d953 caused the xtrace, activated if MSH_FTL_DEBUG is
      set, to be redirected to the output tested by this script,
      causing the check to fail for every shell. The fix is to redirect
      standard error to /dev/null and not to standard output.
    - Also unset MSH_FTL_DEBUG as the xtrace would just waste cycles.

--- lib/_install/good.sh
+++ lib/_install/good.sh
@@ -25,8 +25,8 @@ for MSH_SHELL in "${MSH_SHELL-}" sh /bin/sh ash dash gwsh zsh5 zsh ksh ksh93 lks
 		continue
 	fi
 	case $(	exec "$MSH_SHELL" -c \
-		'case ${ZSH_VERSION+s} in s) emulate sh;; *) (set -o posix) 2>/dev/null && set -o posix;; esac
-		command . "$0" || echo BUG' "${MSH_PREFIX:-$PWD}/lib/modernish/aux/fatal.sh" 2>&1
+		'case ${ZSH_VERSION+s} in s) emulate sh;; *) (set -o posix) && set -o posix;; esac; unset -v MSH_FTL_DEBUG
+		command . "$0" || echo BUG' "${MSH_PREFIX:-$PWD}/lib/modernish/aux/fatal.sh" 2>|/dev/null
 	) in
 	( $$ )	MSH_SHELL=$(command -v "$MSH_SHELL")
 		break ;;
