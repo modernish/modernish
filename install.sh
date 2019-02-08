modernish/modernish

commit d45d4912b842829934e2e8dcd76aa4c840bd8bea

v0.12.1dev: Refactor the fatal bug tests
It was a botch to have fatal bug tests clutter up bin/modernish and
to have separate obfuscated fatal bug tests in {un,}install.sh.
This commit fixes it by refactoring all fatal bug tests so they can
live in a separate and reusable dot script instead.

That script, FTL.t, must be sourced in a certain way (see source)
witin a command substitution that verifies if a success string
($PPID) is output. The script exits on encountering the first bug,
so most tests now do not need their own separate subshell, saving
subshell forks on init. The price is that modernish no longer
reports fatal bugs individually to the user, but that report was
never complete anyway.

libexec/modernish/cap/aux/FTL.t:
- Added.

bin/modernish:
- Remove all fatal bug tests and source FTL.t instead, as above.
- Remove four non-fatal internal bug/quirk tests, as above. The
  only internal cap tests now left are BUG_IFSISSET, KSH88FUNC,
  KSH93FUNC, LOCALVARS, RANDOM and WRN_NOSIGPIPE; their detection
  is too closely intertwined with the initialisation of the library
  to separate them.
- _Msh_have(): remove fatal bug reporting code.
- Try to make dealing with pre-existing aliases & functions even
  less likely to break on interactive shells (some people have
  amazing stuff in their .profile ...)

install.sh, uninstall.sh:
- Remove reduced battery of fatal bug tests; source FTL.t instead.
- No longer bother doing a complete test initialisation of
  modernish in a subshell, but rely on the result of FTL.t.

libexec/modernish/cap/BUG_MULTIBYTE.t,
libexec/modernish/cap/BUG_NOOCTAL.t,
libexec/modernish/cap/BUG_PP_1ARG.t,
libexec/modernish/cap/QRK_IFSFINAL.t:
- Added test scripts for formerly built-in cap IDs.

libexec/modernish/cap/aux/FTL_FLOWCORR.t:
- Removed; now part of FTL.t.

libexec/modernish/tests/string.t:
- Add "whitespace/non-whitespace IFS delimiters" regression test;
  this is the extensive combined QRK_IFSFINAL and fatal IFS

install.sh
@@ -22,6 +22,11 @@
 # OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
 # --- end license ---
 
+# request minimal standards compliance
+POSIXLY_CORRECT=y; export POSIXLY_CORRECT
+std_cmd='case ${ZSH_VERSION+s} in s) emulate sh;; *) (set -o posix) 2>/dev/null && set -o posix;; esac'
+eval "$std_cmd"
+
 # ensure sane default permissions
 umask 022
 
@@ -80,30 +85,24 @@ case ${opt_D+s} in
 ( s )	opt_D=$(mkdir -p "$opt_D" && cd "$opt_D" && pwd && echo X) && opt_D=${opt_D%?X} || exit ;;
 esac
 
-# Since we're running the source-tree copy of modernish and not the
-# installed copy, manually make sure that $MSH_SHELL is a shell with POSIX
-# 'kill -s SIGNAL' syntax and without the cmd subst unbalanced quote bug,
-# FTL_DEVCLOBBR, FTL_ARITHPREC, FTL_PARONEARG, FTL_NOPPID, FTL_FNREDIR,
-# FTL_PSUB, FTL_BRACSQBR, FTL_DEVCLOBBR, FTL_NOARITH, FTL_UPP or FTL_UNSETFAIL.
-# These selected fatal bug tests should lock out most release versions that
-# cannot run modernish. Search these IDs in bin/modernish for documentation.
-test_cmds='test -c /dev/tty >/dev/tty && dev=tty || dev=null
-set -C && : $( : # buggy shells don'\'' tolerate an apostrophe here
-) >"/dev/$dev" && case $((37-16%7+9)) in ( 44 )
-IFS= && set -fCu && set 1 2 3 && set "$@" && [ "$#" -eq 3 ] &&
-f() { echo x; } >&2 && case $(f 2>/dev/null) in ("")
-t=barbarfoo; case ${t##bar*}/${t%%*} in (/)
-t=]abcd; case c in (*["$t"]*) case e in (*[!"$t"]*)
-set -fuC && set -- >/dev/null && kill -s 0 "$$" "$@" && j=0 &&
-unset -v _Msh_foo$((((j+=6*7)==0x2A)>0?014:015)) && echo "$PPID"
-;; esac;; esac;; esac;; esac;; esac'
+# find directory install.sh resides in; assume everything else is there too
+case $0 in
+( */* )	srcdir=${0%/*} ;;
+( * )	srcdir=. ;;
+esac
+srcdir=$(cd "$srcdir" && pwd && echo X) || exit
+srcdir=${srcdir%?X}
+cd "$srcdir" || exit
+
+# find a compliant POSIX shell
+FTL_t=$srcdir/libexec/modernish/cap/aux/FTL.t
 case ${MSH_SHELL-} in
 ( '' )	for MSH_SHELL in sh /bin/sh ash dash zsh5 zsh ksh ksh93 lksh mksh yash bash; do
 		if ! command -v "$MSH_SHELL" >/dev/null; then
 			MSH_SHELL=''
 			continue
 		fi
-		case $(exec "$MSH_SHELL" -c "$test_cmds" 2>/dev/null) in
+		case $(exec "$MSH_SHELL" -c "$std_cmd; command . \"\$0\" || echo BUG" "$FTL_t") in
 		( $$ )	MSH_SHELL=$(command -v "$MSH_SHELL")
 			case ${opt_n+n} in
 			( n )	# If we're non-interactive, relaunch early so that our shell is known.
@@ -120,36 +119,13 @@ case ${MSH_SHELL-} in
 	( '' )	echo "Fatal: can't find any suitable POSIX compliant shell!" 1>&2
 		exit 128 ;;
 	esac
-	case $(eval "$test_cmds" 2>/dev/null) in
-	( '' | *[!0123456789]* )
-		echo "Bug attack! Abandon shell!" >&2
+	case $(command . "$FTL_t" || echo BUG) in
+	( $PPID ) ;;
+	( * )	echo "Bug attack! Abandon shell!" >&2
 		echo "Relaunching ${0##*/} with $MSH_SHELL..." >&2
 		exec "$MSH_SHELL" "$0" "$@" ;;
 	esac ;;
 esac
-
-# Let test initialisations of modernish in other shells use this result.
-export MSH_SHELL
-
-# find directory install.sh resides in; assume everything else is there too
-case $0 in
-( */* )	srcdir=${0%/*} ;;
-( * )	srcdir=. ;;
-esac
-srcdir=$(cd "$srcdir" && pwd && echo X) || exit
-srcdir=${srcdir%?X}
-cd "$srcdir" || exit
-
-# commands for test-initialising modernish
-# test thisshellhas(): a POSIX reserved word, POSIX special builtin, and POSIX regular builtin
-test_modernish='. bin/modernish || exit
-thisshellhas --rw=if --bi=set --bi=wait || exit 1 "Failed to determine a working thisshellhas() function."'
-
-# try to test-initialize modernish in a subshell to see if we can run it
-(eval "$test_modernish") || {
-	echo "install.sh: The shell executing this script was found unable to run modernish."
-	exit 3
-} 1>&2
 
 # load modernish and some modules
 . bin/modernish
@@ -206,7 +182,7 @@ validate_msh_shell() {
 	elif not can exec $msh_shell; then
 		putln "$msh_shell does not seem to be executable. Try another."
 		return 1
-	elif not $msh_shell -c $test_modernish; then
+	elif not str id $$ $(exec $msh_shell -c "$std_cmd; command . \"\$0\" || echo BUG" "$FTL_t"); then
 		putln "$msh_shell was found unable to run modernish. Try another."
 		return 1
 	fi

uninstall.sh
@@ -22,6 +22,11 @@
 # OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
 # --- end license ---
 
+# request minimal standards compliance
+POSIXLY_CORRECT=y; export POSIXLY_CORRECT
+std_cmd='case ${ZSH_VERSION+s} in s) emulate sh;; *) (set -o posix) 2>/dev/null && set -o posix;; esac'
+eval "$std_cmd"
+
 # ensure sane default permissions
 umask 022
 
@@ -53,33 +58,27 @@ case $((OPTIND - 1)) in
 ( * )	usage ;;
 esac
 
-# Since we're running the source-tree copy of modernish and not the
-# installed copy, manually make sure that $MSH_SHELL is a shell with POSIX
-# 'kill -s SIGNAL' syntax and without the cmd subst unbalanced quote bug,
-# FTL_DEVCLOBBR, FTL_ARITHPREC, FTL_PARONEARG, FTL_NOPPID, FTL_FNREDIR,
-# FTL_PSUB, FTL_BRACSQBR, FTL_DEVCLOBBR, FTL_NOARITH, FTL_UPP or FTL_UNSETFAIL.
-# These selected fatal bug tests should lock out most release versions that
-# cannot run modernish. Search these IDs in bin/modernish for documentation.
-test_cmds='test -c /dev/tty >/dev/tty && dev=tty || dev=null
-set -C && : $( : # buggy shells don'\'' tolerate an apostrophe here
-) >"/dev/$dev" && case $((37-16%7+9)) in ( 44 )
-IFS= && set -fCu && set 1 2 3 && set "$@" && [ "$#" -eq 3 ] &&
-f() { echo x; } >&2 && case $(f 2>/dev/null) in ("")
-t=barbarfoo; case ${t##bar*}/${t%%*} in (/)
-t=]abcd; case c in (*["$t"]*) case e in (*[!"$t"]*)
-set -fuC && set -- >/dev/null && kill -s 0 "$$" "$@" && j=0 &&
-unset -v _Msh_foo$((((j+=6*7)==0x2A)>0?014:015)) && echo "$PPID"
-;; esac;; esac;; esac;; esac;; esac'
+# find directory uninstall.sh resides in; assume everything else is there too
+case $0 in
+( */* )	srcdir=${0%/*} ;;
+( * )	srcdir=. ;;
+esac
+srcdir=$(cd "$srcdir" && pwd -P && echo X) || exit
+srcdir=${srcdir%?X}
+cd "$srcdir" || exit
+
+# find a compliant POSIX shell
 case ${MSH_SHELL-} in
-( '' )	if command -v modernish >/dev/null; then
-		IFS="#!$IFS" read junk junk MSH_SHELL junk <"$(command -v modernish)"
+( '' )	FTL_t=$srcdir/libexec/modernish/cap/aux/FTL.t
+	if command -v modernish >/dev/null; then
+		read -r MSH_SHELL <"$(command -v modernish)" 2>/dev/null && MSH_SHELL=/${MSH_SHELL#*/}
 	fi
 	for MSH_SHELL in "$MSH_SHELL" sh /bin/sh ash dash zsh5 zsh ksh ksh93 lksh mksh yash bash; do
 		if ! command -v "$MSH_SHELL" >/dev/null; then
 			MSH_SHELL=''
 			continue
 		fi
-		case $(exec "$MSH_SHELL" -c "$test_cmds" 2>/dev/null) in
+		case $(exec "$MSH_SHELL" -c "$std_cmd; command . \"\$0\" || echo BUG" "$FTL_t") in
 		( $$ )	MSH_SHELL=$(command -v "$MSH_SHELL")
 			export MSH_SHELL
 			break ;;
@@ -91,35 +90,13 @@ case ${MSH_SHELL-} in
 	( '' )	echo "Fatal: can't find any suitable POSIX compliant shell!" 1>&2
 		exit 128 ;;
 	esac
-	case $(eval "$test_cmds" 2>/dev/null) in
-	( '' | *[!0123456789]* )
-		echo "Bug attack! Abandon shell!" >&2
+	case $(command . "$FTL_t" || echo BUG) in
+	( $PPID ) ;;
+	( * )	echo "Bug attack! Abandon shell!" >&2
 		echo "Relaunching ${0##*/} with $MSH_SHELL..." >&2
-		exec "$MSH_SHELL" "$0" "$@" ;;
-	esac ;;
-( * )	case $(exec "$MSH_SHELL" -c "$test_cmds" 2>/dev/null) in
-	( $$ )	;;
-	( * )	echo "Shell $MSH_SHELL is not a suitable POSIX compliant shell." >&2
-		exit 1 ;;
+		exec "$MSH_SHELL" "$0" --relaunch "$@" ;;
 	esac ;;
 esac
-
-# find directory uninstall.sh resides in; assume everything else is there too
-case $0 in
-( */* )	srcdir=${0%/*} ;;
-( * )	srcdir=. ;;
-esac
-srcdir=$(cd "$srcdir" && pwd -P && echo X) || exit
-srcdir=${srcdir%?X}
-cd "$srcdir" || exit
-
-# try to test-initialize modernish in a subshell to see if we can run it
-if ! (eval ". bin/modernish"); then
-	echo
-	echo "The shell executing this script can't run modernish. Try running uninstall.sh"
-	echo "with a more fully POSIX-compliant shell, for instance: dash uninstall.sh"
-	exit 3
-fi 1>&2
 
 # load modernish and some modules
 . bin/modernish

libexec/modernish/cap/BUG_MULTIBYTE.t
@@ -1,0 +1,23 @@
+#! /shell/bug/test/for/moderni/sh
+# See the file LICENSE in the main modernish directory for the licence.
+
+# BUG_MULTIBYTE: We're running on a locale with a variable-length character
+# set (i.e. UTF-8) but the shell does not support multi-byte characters. For
+# instance, ${#var} measures length in bytes, not characters. With
+# fixed-length one-byte character sets, the bug is irrelevant so we don't
+# set the identifier. Current shells with this bug include dash and most
+# branches of pdksh.
+# Note: Currently, BUG_MULTIBYTE is only detected if we're in a UTF-8 locale.
+# (This is the only multibyte locale supported by current shells.)
+# It should not be detected for single-byte locales as it's irrelevant there.
+# Ref.: http://pubs.opengroup.org/onlinepubs/9699919799/utilities/V3_chap02.html#tag_18_06_02
+
+case ${LC_ALL:-${LC_CTYPE:-${LANG:-}}} in
+( *.[Uu][Tt][Ff]8 | *.[Uu][Tt][Ff]-8 )
+	_Msh_test='bèta' # 4 char, 5 byte UTF-8 string 'beta' with accent grave on 'e'
+	case ${#_Msh_test} in
+	( 5 )	;;
+	( * )	return 1 ;;
+	esac ;;
+( * )	return 1 ;;
+esac

libexec/modernish/cap/BUG_NOOCTAL.t
@@ -1,0 +1,11 @@
+#! /shell/bug/test/for/moderni/sh
+# See the file LICENSE in the main modernish directory for the licence.
+
+# BUG_NOOCTAL: Shell arithmetic does interpret numbers with leading
+# zeroes as octal numbers; these are interpreted as decimal instead,
+# though POSIX specifies octal. (older mksh, 2013-ish versions)
+
+case $((010)) in
+( 10 )	;;
+( * )	return 1 ;;
+esac

libexec/modernish/cap/BUG_PP_1ARG.t
@@ -1,0 +1,17 @@
+#! /shell/bug/test/for/moderni/sh
+# See the file LICENSE in the main modernish directory for the licence.
+
+# BUG_PP_1ARG: When IFS is empty on bash <= 4.3 (i.e. field
+# splitting is off), ${1+"$@"} or "${1+$@}" is counted as a single
+# argument instead of each positional parameter as separate arguments.
+# This also applies to prepending text only if there are positional
+# parameters with something like "${1+foobar $@}".
+set -- "   \on\e" "\tw'o" " \th\'re\e" " \\'fo\u\r "
+push IFS
+IFS=''
+set -- ${1+"$@"}
+pop IFS
+case $# in
+( 1 )	;;
+( * )	return 1 ;;
+esac

libexec/modernish/cap/QRK_IFSFINAL.t
@@ -1,0 +1,20 @@
+#! /shell/quirk/test/for/moderni/sh
+# See the file LICENSE in the main modernish directory for the licence.
+
+# QRK_IFSFINAL: in field splitting, a final non-whitespace IFS delimiter
+# character is counted as an empty field (yash < 2.42, zsh, pdksh). Modernish
+# designates this a QRK (quirk), not a BUG, because POSIX is ambiguous on
+# this, and it's really not clear that the behaviour is undesirable.
+# Nonetheless it seems the dominant interpretation considers it a bug.
+# Ref.: http://www.open-std.org/JTC1/SC22/WG15/docs/rr/9945-2/9945-2-98.html
+#	https://osdn.net/projects/yash/ticket/35283#comment:3863:35283:1435293070
+
+_Msh_test=one/two/
+push IFS
+IFS=/
+set -- ${_Msh_test}
+pop IFS
+case $# in
+( 3 )	;;
+( * )	return 1 ;;
+esac

commit 925f77664c1ee340eada495610e8e2305134c3bb

(un)installer: fix relaunching if PWD is not the script's directory
install.sh, uninstall.sh:
- We were relaunching self using $0 after cd'ing into the source
  directory, which failed if the script was invoked from another
  directory. Use $srcdir for the directory component instead.
  Fixes bug introduced by d45d4912.

install.sh
@@ -105,13 +105,13 @@ case ${MSH_SHELL-} in
 	case ${opt_n+n} in
 	( n )	# If we're non-interactive, relaunch early so that our shell is known.
 		echo "Relaunching ${0##*/} with $MSH_SHELL..." >&2
-		exec "$MSH_SHELL" "$0" --relaunch "$@" ;;
+		exec "$MSH_SHELL" "$srcdir/${0##*/}" --relaunch "$@" ;;
 	esac
 	case $(command . lib/modernish/aux/fatal.sh || echo BUG) in
 	( "${PPID:-no_match_on_no_PPID}" ) ;;
 	( * )	echo "Bug attack! Abandon shell!" >&2
 		echo "Relaunching ${0##*/} with $MSH_SHELL..." >&2
-		exec "$MSH_SHELL" "$0" "$@" ;;	# no --relaunch or we'll skip the menu
+		exec "$MSH_SHELL" "$srcdir/${0##*/}" "$@" ;;	# no --relaunch or we'll skip the menu
 	esac ;;
 esac
 

uninstall.sh
@@ -82,7 +82,7 @@ case ${MSH_SHELL-} in
 	( "${PPID:-no_match_on_no_PPID}" ) ;;
 	( * )	echo "Bug attack! Abandon shell!" >&2
 		echo "Relaunching ${0##*/} with $MSH_SHELL..." >&2
-		exec "$MSH_SHELL" "$0" --relaunch "$@" ;;
+		exec "$MSH_SHELL" "$srcdir/${0##*/}" --relaunch "$@" ;;
 	esac ;;
 esac
 