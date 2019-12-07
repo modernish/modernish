9db8cf905ea97234dcf29488db4efc8e35491a1d
Martijn Dekker <martijn@inlv.org>
2019-12-07 18:56:59 +0100

    tst/run.sh: fix hardened 'mkdir' default permissions
    I had mistakenly assumed the '-m' option takes zero permissions as
    default so the '-m' option needs to add to it. In fact, POSIX says
    that it takes 'a=rwx' (i.e., 777) as default. So to make a private
    user-only directory, we need 'mkdir -m go-rwx'.

--- lib/modernish/tst/run.sh
+++ lib/modernish/tst/run.sh
@@ -161,7 +161,7 @@ fi
 # Harden utilities used below and in tests, searching them in the system default PATH.
 harden -pP cat
 harden -p ln
-harden -p mkdir -m u+rwx
+harden -p mkdir -m go-rwx
 harden -p pr
 harden -p rm
 harden -p sed
