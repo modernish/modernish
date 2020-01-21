17634e22679967f8c1dd24675d49bae2e2dbfc9b
Martijn Dekker <martijn@inlv.org>
2020-01-21 14:26:55 +0100

    var/loop/find: fix for 'find' expression: -ok ... {} +
    lib/modernish/aux/var/loop/find-ok.sh:
    - Bugfix: Support multiple arguments, not just one. Some 'find'
      implementations can use '-ok somecommand {} +' as an extension,
      so this script needs to support that.
    - Reduce excessive comments.
    
    lib/modernish/mdl/var/loop/find.mm:
    - Bugfix: When --xargs is combined with -ok or -okdir, pass
      arguments to find-ok.sh in batches ('{} +').

--- lib/modernish/mdl/var/loop/find.mm
+++ lib/modernish/mdl/var/loop/find.mm
@@ -417,8 +417,12 @@ _loopgen_find() {
 # Internal helper function to determine what translation of -iterate to use.
 # -i designates version for interactive use: one file name per invocation.
 _loop_find_setIter() {
-	if str eq ${1-} -i && not isset _loop_xargs && is onterminal stdin; then
-		_loop_iter='-exec $MSH_SHELL $MSH_AUX/var/loop/find-ok.sh {} \;'
+	if str eq ${1-} -i && is onterminal stdin; then
+		if isset _loop_xargs; then
+			_loop_iter='-exec $MSH_SHELL $MSH_AUX/var/loop/find-ok.sh {} +'
+		else
+			_loop_iter='-exec $MSH_SHELL $MSH_AUX/var/loop/find-ok.sh {} \;'
+		fi
 	else
 		_loop_iter='-exec $MSH_SHELL $MSH_AUX/var/loop/find.sh {} +'
 	fi
