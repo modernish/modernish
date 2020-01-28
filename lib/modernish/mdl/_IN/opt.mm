modernish/modernish

commit 45375da9ec586dec693317b5793e4886bcbb7e63

use(): minor bugfix; force subshell fork on ksh93
bin/modernish: use():
- Replace broken method to ensure 'set -a' (-o allexport) isn't
  active during module initialisation. Use the stack.
- On ksh93, if we're loading a module in a subshell, force a fork
  to avoid masses of problems related to non-forking subshells.

bin/modernish
@@ -540,21 +540,25 @@ use() {
 		is reg "$MSH_MDL/$1.mm" || is dir "$MSH_MDL/$1"
 		return ;;
 	esac
+	push --key=_Msh_use -a
+	set +a  # don't export module vars
 	_Msh_doUse "$@"
 	case $? in
 	( 0 )	if isset -i && ! str begin "$1" '_IN/'; then putln "Using $1" >&2; fi
 		_Msh_using=${_Msh_using}${_Msh_using:+$CCn}$1 ;;
-	( * )	die "use: initialisation of module $1 failed" ;;
+	( * )	pop --key=_Msh_use -a; die "use: initialisation of module $1 failed" ;;
 	esac
+	pop --key=_Msh_use --keepstatus -a
 }
 _Msh_doUse() {
+	case ${KSH_VERSION-} in
+	( 'Version '* | 2[0-9][0-9][0-9].* )
+		# If a module is initialised in a subshell on ksh93, force it to fork to avoid many bugs.
+		# Ref.: https://github.com/att/ast/issues/480
+		let .sh.subshell && command ulimit -t unlimited 2>/dev/null ;;
+	esac
 	if is reg "$MSH_MDL/$1.mm"; then
-		case $- in
-		( *a* )	set +a
-			. "$MSH_MDL/$1.mm" >&2
-			eval "set -a; return $?" ;;
-		( * )	. "$MSH_MDL/$1.mm" >&2 ;;
-		esac
+		. "$MSH_MDL/$1.mm" >&2
 	elif is dir "$MSH_MDL/$1"; then
 		let "$# == 1" || die "use: arguments cannot be passed to module directory: $1"
 		# recursively initialise all modules...

commit 02b501bf2f7146a32aa5e0472d2bc32255dd35da

use(): bugfix: don't use 'push'/'pop', as push/pop may call 'use'
bin/modernish:
- use(): Fix bug introduced in 45375da9 that could cause 'push' to
  be in an inconsistent state when using 'push -o'.

lib/_install/bin/modernish.bundle.diff:
- Update.

lib/modernish/mdl/_IN/opt.mm:
- Add reminder comment not to use push/pop in init.

bin/modernish
@@ -511,6 +511,12 @@ setstatus() {
 # Option -l: list modules in use.
 _Msh_using=''
 use() {
+	case $- in
+	( *a* )	# Make sure modules are initialised without allexport.
+		# Note: module init must never 'set -a', or this will break on recursive module init.
+		# (We can't use 'push -a'/'pop -a' here, because 'push'/'pop' may itself call 'use'.)
+		set +a; use "$@"; eval "set -a; return $?" ;;
+	esac
 	unset -v _Msh_use_o
 	while	case ${1-} in
 		( -- )		shift; break ;;
@@ -547,15 +553,9 @@ use() {
 		is reg "$MSH_MDL/$1.mm" || is dir "$MSH_MDL/$1"
 		return ;;
 	esac
-	push --key=_Msh_use -a
-	set +a  # don't export module vars
-	_Msh_doUse "$@"
-	case $? in
-	( 0 )	if isset -i && ! str begin "$1" '_IN/' && ! insubshell; then putln "Using $1" >&2; fi
-		_Msh_using=${_Msh_using}${_Msh_using:+$CCn}$1 ;;
-	( * )	pop --key=_Msh_use -a; die "use: initialisation of module $1 failed" ;;
-	esac
-	pop --key=_Msh_use --keepstatus -a
+	_Msh_doUse "$@" || die "use: initialisation of module $1 failed"
+	if isset -i && ! str begin "$1" '_IN/' && ! insubshell; then putln "Using $1" >&2; fi
+	_Msh_using=${_Msh_using}${_Msh_using:+$CCn}$1
 }
 _Msh_doUse() {
 	case ${KSH_VERSION-} in

lib/_install/bin/modernish.bundle.diff
@@ -1,5 +1,5 @@
---- bin/modernish	2020-01-28 05:22:45.000000000 +0100
-+++ /private/tmp/temp.pn0%@x@A.v/patched_bin_modernish	2020-01-28 05:29:57.000000000 +0100
+--- bin/modernish	2020-01-28 22:42:00.000000000 +0100
++++ /tmp/temp.x138^Z7S:W/patched_bin_modernish	2020-01-28 22:44:13.000000000 +0100
 @@ -30,49 +30,7 @@
  # -------------
  
@@ -185,16 +185,15 @@
  
  # Extended 'exit'. Usage: exit [ -u ] [ <status> [ <message> ] ]
  # The <status> is a shell arithmetic expression.
-@@ -551,7 +427,7 @@
- 	set +a  # don't export module vars
- 	_Msh_doUse "$@"
- 	case $? in
--	( 0 )	if isset -i && ! str begin "$1" '_IN/' && ! insubshell; then putln "Using $1" >&2; fi
-+	( 0 )
- 		_Msh_using=${_Msh_using}${_Msh_using:+$CCn}$1 ;;
- 	( * )	pop --key=_Msh_use -a; die "use: initialisation of module $1 failed" ;;
+@@ -554,7 +430,6 @@
+ 		return ;;
  	esac
-@@ -611,35 +487,12 @@
+ 	_Msh_doUse "$@" || die "use: initialisation of module $1 failed"
+-	if isset -i && ! str begin "$1" '_IN/' && ! insubshell; then putln "Using $1" >&2; fi
+ 	_Msh_using=${_Msh_using}${_Msh_using:+$CCn}$1
+ }
+ _Msh_doUse() {
+@@ -611,35 +486,12 @@
  _Msh_cap=''
  
  # ** First, some internal functions to support thisshellhas():
@@ -234,7 +233,7 @@
  }
  
  # ** Shell-specific code for thisshellhas():
-@@ -820,16 +673,6 @@
+@@ -820,16 +672,6 @@
  	esac
  	while case $# in (0) break;; esac; do
  		case $1 in
@@ -251,7 +250,7 @@
  		( "" | --bi= | --[rk]w= | --bi=*/* | --[rk]w=*/* \
  		| --bi=*[!\[\]\!{}"$SHELLSAFECHARS"]* \
  		| --[rk]w=*[!\[\]\!{}"$SHELLSAFECHARS"]* \
-@@ -877,7 +720,7 @@
+@@ -877,7 +719,7 @@
  			;;
  		( * )	case " ${_Msh_cap} " in
  			( *" $1 "* )	;;
@@ -260,7 +259,7 @@
  			( * )	_Msh_doCapTest "$1"
  				case $? in
  				( 0 )	_Msh_cap=${_Msh_cap:+${_Msh_cap} }$1
-@@ -2183,16 +2026,6 @@
+@@ -2183,16 +2025,6 @@
  
  # --- End of core library, start of initialisation phase 2 ---
  
@@ -277,7 +276,7 @@
  # If shell supports it, then set modernish functions to read-only.
  if thisshellhas ROFUNC; then
  	readonly -f \
-@@ -2281,22 +2114,12 @@
+@@ -2281,22 +2113,12 @@
  if ! str end "$0" '/modernish' && ! str eq "$0" 'modernish'; then
  
  	# --- modernish was sourced (simple use) ---
@@ -300,7 +299,7 @@
  	unset -f _Msh_tmp_doHashbangPreload
  	# Restore 'allexport' option if it was set
  	if isset _Msh_allexport; then
-@@ -2401,16 +2224,6 @@
+@@ -2401,16 +2223,6 @@
  			}
  			_Msh_tmp_doUse "${1#--use=}"
  			unset -f _Msh_tmp_doUse ;;
@@ -317,7 +316,7 @@
  		( --version | --help )
  			use sys/base/readlink
  			readlink -es "$MSH_SHELL" && str ne "$REPLY" "$MSH_SHELL" || unset -v REPLY
-@@ -2420,7 +2233,6 @@
+@@ -2420,7 +2232,6 @@
  			      "Known-good shell:	MSH_SHELL=$MSH_SHELL${REPLY:+ -> $REPLY}" \
  			      "Default utility PATH:	DEFPATH=$DEFPATH" \
  			      "User config directory:	MSH_CONFIG=$MSH_CONFIG"

lib/modernish/mdl/_IN/opt.mm
@@ -178,6 +178,8 @@ _Msh_optNamToVar() {
 # -------------------
 # --- Module init ---
 
+# Note: 'push' and 'pop' may initialise this module, so never use 'push' and 'pop' in init here.
+
 # Initialise a list of all the shell's canonical long-form option names.
 # These are extracted from 'set -o' output. Format unspecified by POSIX, but parseable in practice.
 unset -v _Msh_allMyLongOpts