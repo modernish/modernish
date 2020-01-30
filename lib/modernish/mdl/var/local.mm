a99e5de7b439857e5e455ebe33ce4b53ef475f90
Martijn Dekker <martijn@inlv.org>
2020-01-31 00:06:20 +0100

    LINENO: fix detection; make compatible with bundling
    This partially reverts commit 1b083763. On second thought, not
    detecting LINENO if BUG_LNNONEG is detected is misguided; a broken
    feature is not an absent feature. This also made regression-testing
    the detection needlessly complicted, so that was broken, too.
    
    lib/modernish/cap/LINENO.t:
    - Once again detect LINENO regardless of value validity.
    
    lib/modernish/mdl/var/local.mm,
    lib/modernish/mdl/var/loop.mm:
    - Don't report line number in errmsgs if thisshellhas BUG_LNNONEG.
    
    lib/modernish/tst/builtin.t:
    - Separate regresson tests for LINENO and BUG_LNNONEG detection.

--- lib/modernish/mdl/var/local.mm
+++ lib/modernish/mdl/var/local.mm
@@ -77,7 +77,8 @@ isset -i && use var/stack/trap
 # The aliases below pass $LINENO on to the handling functions for use in error messages, so they can report
 # the line number of the 'LOCAL' or 'END' where the error occurred. But on shells with BUG_LNNOALIAS
 # (pdksh, mksh) this is pointless as the number is always zero when $LINENO is expanded from an alias.
-if not thisshellhas LINENO || thisshellhas BUG_LNNOALIAS; then
+# And on shells with BUG_LNNONEG (dash), the results would often be wildly inaccurate.
+if not thisshellhas LINENO || thisshellhas BUG_LNNOALIAS || thisshellhas BUG_LNNONEG; then
 	_Msh_sL_LINENO="''"
 else
 	_Msh_sL_LINENO='"${LINENO-}"'
